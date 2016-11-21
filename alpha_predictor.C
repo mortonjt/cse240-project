ac3a92f51ec8b1e51f200e88a0ca422f05878887
Jamie Morton <jamietmorton@gmail.com>
2016-11-20 21:58:50 -0800

    Correcting handling for choice and global predictors

--- alpha_predictor.C
+++ alpha_predictor.C
@@ -19,9 +19,9 @@ Budget sizes
 1M   + 4K  -> k=12, s=7,  t=18
 */
 
-# define k 12   // bits for hashing local branches
-# define s 6    // bits for history
-# define t 18   // bits for global/choice history
+# define k 10   // bits for hashing local branches
+# define s 3    // bits for history
+# define t 12   // bits for global/choice history
 
 /*
 Stores local history table
@@ -29,9 +29,8 @@ Stores local history table
 Maximum size
 k=12 -> 4096  = 2^12
 s=8  -> 256   = 2^8
-t=14 -> 16384 = 2^14
  */
-unsigned int local_pattern_table[4096][256];
+unsigned int local_pattern_table[4096][4096];
 
 /*
 Histories for each branch
@@ -57,6 +56,10 @@ t=18 -> 2^18
 */
 unsigned int choice_pattern_table[262144];
 
+/*
+path history
+*/
+unsigned int path_history;
 
 // Helper functions
 void local_step(int i, int j, bool outcome){
@@ -97,12 +100,18 @@ void local_step(int i, int j, bool outcome){
   } else if(local_pattern_table[i][j] == SNT and outcome == false){
     local_pattern_table[i][j] = SNT;
   }
-
-  // Update pattern_table
+  // Update local histories table
   local_histories[i] =  (local_histories[i] << 1) | outcome;
 }
 
 void global_step(int i, bool outcome){
+  /*
+    Updates global pattern table.
+
+    Parameters
+    ----------
+    i : the hash of the pattern corresponding to the last t occurences
+   */
   // Strongly taken
   if(global_pattern_table[i] == ST and outcome == true){
     global_pattern_table[i] = ST;
@@ -130,32 +139,52 @@ void global_step(int i, bool outcome){
 }
 
 
-void choice_step(int i, bool outcome){
-  // Strongly taken
-  if(choice_pattern_table[i] == ST and outcome == true){
-    choice_pattern_table[i] = ST;
-  } else if(choice_pattern_table[i] == ST and outcome == false){
-    choice_pattern_table[i] = WT;
-  }
-  // Weakly taken
-  else if(choice_pattern_table[i] == WT and outcome == true){
-    choice_pattern_table[i] = ST;
-  } else if(choice_pattern_table[i] == WT and outcome == false){
-    choice_pattern_table[i] = WNT;
-  }
-  // Weakly not taken
-  else if(choice_pattern_table[i] == WNT and outcome == true){
-    choice_pattern_table[i] = WT;
-  } else if(choice_pattern_table[i] == WNT and outcome == false){
-    choice_pattern_table[i] = SNT;
-  }
-  // Strongly not taken
-  else if(choice_pattern_table[i] == SNT and outcome == true){
-    choice_pattern_table[i] = WNT;
-  } else if(choice_pattern_table[i] == SNT and outcome == false){
-    choice_pattern_table[i] = SNT;
-  }
+void choice_step(int i,
+		 bool global_outcome,
+		 bool local_outcome,
+		 bool outcome){
+  /*
+    Updates global pattern table.
 
+    Parameters
+    ----------
+    i : the hash of the pattern corresponding to the last t occurences
+   */
+
+  // Strongly taken (local)
+  if(choice_pattern_table[i] == ST and
+     ((local_outcome == true and outcome == false) or
+      (local_outcome == false and outcome == true))){
+    choice_pattern_table[i] = WT;
+  }
+  // Weakly taken (local)
+  else if(choice_pattern_table[i] == WT and
+	  ((local_outcome == false and outcome == true) or
+	   (local_outcome == true and outcome == false))){
+    choice_pattern_table[i] = WNT;
+  }
+  else if(choice_pattern_table[i] == WT and
+	  ((local_outcome == true and outcome == true) or
+	   (local_outcome == false and outcome == false))){
+    choice_pattern_table[i] = ST;
+  }
+  // Weakly not taken (global)
+  else if(choice_pattern_table[i] == WNT and
+	  ((global_outcome == false and outcome == true) or
+	   (global_outcome == true and outcome == false))){
+    choice_pattern_table[i] = WT;
+  }
+  else if(choice_pattern_table[i] == WNT and
+	  ((global_outcome == false and outcome == false) or
+	   (global_outcome == true and outcome == true))){
+    choice_pattern_table[i] = SNT;
+  }
+  // Strongly not taken (global)
+  else if(choice_pattern_table[i] == SNT and
+	  ((global_outcome == true and outcome == false) or
+	   (global_outcome == false and outcome == true))){
+    choice_pattern_table[i] = WNT;
+  }
 }
 
 
@@ -169,21 +198,24 @@ void init_predictor ()
     }
     // initialize all histories to zero.
     local_histories[i] = 0;
+  }
+  for (int i = 0; i<pow(2, t); i++){
     global_pattern_table[i] = 0;
     choice_pattern_table[i] = 0;
   }
+  path_history = 0;
 }
 
 
 bool make_prediction (unsigned int pc)
 {
-
+  //printf ("Predicting\n");
   int branch = (pc % k);  // branch hash
-  int global_idx = (pc % t);  // global pc hash
   int history_idx = local_histories[branch] % s;
   int local_taken = local_pattern_table[branch][history_idx];
-  int global_taken = global_pattern_table[global_idx];
-  int choice_taken = choice_pattern_table[global_idx];
+  int path_idx = path_history % t;
+  int global_taken = global_pattern_table[path_idx];
+  int choice_taken = choice_pattern_table[path_idx];
 
   int taken = 0;
   if(choice_taken == ST or choice_taken == WT){
@@ -191,6 +223,8 @@ bool make_prediction (unsigned int pc)
   }else{
     taken = global_taken;
   }
+
+  taken = global_taken;
 
   if(taken == ST or taken == WT)
     return true;
@@ -201,12 +235,18 @@ bool make_prediction (unsigned int pc)
 
 void train_predictor (unsigned int pc, bool outcome)
 {
-
   int branch = (pc % k);  // branch hash
-  int local_history_idx = local_histories[branch] % s;
-  int global_idx = (pc % t);  // global pc hash
-  local_step(branch, local_history_idx, outcome);
-  global_step(global_idx, outcome);
-  choice_step(global_idx, outcome);
+  int history_idx  = local_histories[branch] % s;
+  int path_idx = path_history % t;
+  int local_taken  = local_pattern_table[branch][history_idx];
+  int global_taken = global_pattern_table[path_idx];
+  int choice_taken = choice_pattern_table[path_idx];
 
+  choice_step(path_idx, global_taken, local_taken, outcome);
+  global_step(path_idx, outcome);
+  local_step(branch, history_idx, outcome);
+
+
+  // Update overall path history
+  path_history =  (path_history << 1) | outcome;
 }
