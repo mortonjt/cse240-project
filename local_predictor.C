ea227d0b0bb2e4f26d2a9716e8c9da3a4fa3a2cd
Jamie Morton <jamietmorton@gmail.com>
2016-11-21 00:06:27 -0800

    refactor

--- local_predictor.C
+++ local_predictor.C
@@ -7,6 +7,8 @@
 # define WNT 2
 # define SNT 3
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 /*
 Budget sizes
@@ -19,8 +21,8 @@ Budget sizes
 1M   + 4K  -> k=12, s=8
 */
 
-# define k 12 // number of bits for hashing branches
-# define s 8 // 4096=2^12.  12 bits for history
+# define k 8 // number of bits for hashing branches
+# define s 12 // 4096=2^12.  12 bits for history
 
 
 /*
@@ -30,7 +32,7 @@ Maximum size
 k=12 -> 4096 = 2^12
 s=8  -> 256  = 2^8
  */
-unsigned int pattern_table[4096][256];
+unsigned int pattern_table[4096][4096];
 
 /*
 Histories for each branch
@@ -39,52 +41,7 @@ Maximum size
 k=12 -> 4096 = 2^12
 */
 unsigned int histories[4096];
-//int histories[64];
-
-// Helper functions
-void step(int i, int j, bool outcome){
-  /*
-    This method will update the 2bit saturating counter at branch i
-    and history j.
-
-    Parameters
-    ----------
-    i : row in the global history table.
-	i.e. the branch that is being inspected
-    j : column in the global history table.
-	i.e. the history that is being inspected for branch i.
-    outcome : outcome of specific branch at that given time
-   */
-
-  // Strongly taken
-  if(pattern_table[i][j] == ST and outcome == true){
-    pattern_table[i][j] = ST;
-  } else if(pattern_table[i][j] == ST and outcome == false){
-    pattern_table[i][j] = WT;
-  }
-  // Weakly taken
-  else if(pattern_table[i][j] == WT and outcome == true){
-    pattern_table[i][j] = ST;
-  } else if(pattern_table[i][j] == WT and outcome == false){
-    pattern_table[i][j] = WNT;
-  }
-  // Weakly not taken
-  else if(pattern_table[i][j] == WNT and outcome == true){
-    pattern_table[i][j] = WT;
-  } else if(pattern_table[i][j] == WNT and outcome == false){
-    pattern_table[i][j] = SNT;
-  }
-  // Strongly not taken
-  else if(pattern_table[i][j] == SNT and outcome == true){
-    pattern_table[i][j] = WNT;
-  } else if(pattern_table[i][j] == SNT and outcome == false){
-    pattern_table[i][j] = SNT;
-  }
-
-  // Update history
-  histories[i] =  (histories[i] << 1) | outcome;
-}
-
+unsigned int history;
 
 void init_predictor ()
 {
@@ -101,11 +58,9 @@ void init_predictor ()
 
 bool make_prediction (unsigned int pc)
 {
-
   int branch = (pc % k);  // branch hash
-  int history_idx = histories[branch] % s;
-  int taken = pattern_table[branch][history_idx];
-  if(taken == ST or taken == WT)
+  int i = histories[branch];
+  if(pattern_table[branch][i] < 2)
     return true;
   else
     return false;
@@ -116,7 +71,16 @@ void train_predictor (unsigned int pc, bool outcome)
 {
 
   int branch = (pc % k);  // branch hash
-  int history_idx = histories[branch] % s;
-  step(branch, history_idx, outcome);
+  unsigned int i = histories[branch];
+  if (pattern_table[branch][i] < 2 and outcome){
+    pattern_table[branch][i] = ST;
+  }else if (pattern_table[branch][i] > 2 and not outcome){
+    pattern_table[branch][i] = SNT;
+  }else if (outcome){
+    pattern_table[branch][i] = MAX(0, pattern_table[branch][i]-1);
+  }else{
+    pattern_table[branch][i] = MIN(3, pattern_table[branch][i]+1);
+  }
 
+  histories[i] =  ((histories[i] << 1) | outcome) % s;
 }
