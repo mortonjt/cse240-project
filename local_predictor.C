ed4e4e580b6da9677a040a09ee87ba6915a0cd37
Jamie Morton <jamietmorton@gmail.com>
2016-11-21 15:21:13 -0800

    Adding changes to local predictor
    Only have two 1D tables now

--- local_predictor.C
+++ local_predictor.C
@@ -21,8 +21,8 @@ Budget sizes
 1M   + 4K  -> k=12, s=8
 */
 
-# define k 8 // number of bits for hashing branches
-# define s 12 // 4096=2^12.  12 bits for history
+# define k 5 // number of bits for hashing branches
+# define s 19 // 4096=2^12.  12 bits for history
 
 
 /*
@@ -32,7 +32,8 @@ Maximum size
 k=12 -> 4096 = 2^12
 s=8  -> 256  = 2^8
  */
-unsigned int pattern_table[4096][4096];
+unsigned int pattern_table[1048576];
+
 
 /*
 Histories for each branch
@@ -40,18 +41,18 @@ Histories for each branch
 Maximum size
 k=12 -> 4096 = 2^12
 */
-unsigned int histories[4096];
+unsigned int histories[1048576];
 unsigned int history;
 
 void init_predictor ()
 {
   // initialize all 2bit saturating counts to Strongly Taken
   for (int i = 0; i<pow(2, k); i++){
-    for (int j = 0; j<pow(2, s); j++){
-      pattern_table[i][j] = 0;
-    }
     // initialize all histories to zero.
     histories[i] = 0;
+  }
+  for (int j = 0; j<pow(2, s); j++){
+    pattern_table[j] = 0;
   }
 }
 
@@ -60,7 +61,7 @@ bool make_prediction (unsigned int pc)
 {
   int branch = (pc % k);  // branch hash
   int i = histories[branch];
-  if(pattern_table[branch][i] < 2)
+  if(pattern_table[i] < 2)
     return true;
   else
     return false;
@@ -72,14 +73,17 @@ void train_predictor (unsigned int pc, bool outcome)
 
   int branch = (pc % k);  // branch hash
   unsigned int i = histories[branch];
-  if (pattern_table[branch][i] < 2 and outcome){
-    pattern_table[branch][i] = ST;
-  }else if (pattern_table[branch][i] > 2 and not outcome){
-    pattern_table[branch][i] = SNT;
-  }else if (outcome){
-    pattern_table[branch][i] = MAX(0, pattern_table[branch][i]-1);
-  }else{
-    pattern_table[branch][i] = MIN(3, pattern_table[branch][i]+1);
+  unsigned int state = pattern_table[i];
+  if (state < 2 and outcome){
+    pattern_table[i] = ST;
+  }else if (state >= 2 and not outcome){
+    pattern_table[i] = SNT;
+  }else if (state >= 2 and outcome){
+    pattern_table[i] = MAX(0, state-1);
+  }else if (state < 2 and not outcome){
+    pattern_table[i] = MIN(3, state+1);
+  } else{
+    printf("warning");
   }
 
   histories[i] =  ((histories[i] << 1) | outcome) % s;
