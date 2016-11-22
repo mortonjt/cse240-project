a985ee5b6ca4d762450ce169cef237cccc240c48
Jamie Morton <jamietmorton@gmail.com>
2016-11-21 20:08:49 -0800

    Refactor

--- local_predictor.C
+++ local_predictor.C
@@ -13,41 +13,39 @@
 /*
 Budget sizes
 
-8K   + 64  -> k=6,  s=7
-16K  + 128 -> k=7,  s=7
-32K  + 256 -> k=8,  s=7
-64K  + 512 -> k=9,  s=7
-128K + 1K  -> k=10, s=7
-1M   + 4K  -> k=12, s=8
+8K   + 64  -> k=10,  s=7
+16K  + 128 -> k=11,  s=7
+32K  + 256 -> k=12,  s=7
+64K  + 512 -> k=13,  s=7
+128K + 1K  -> k=14,  s=8
+1M   + 4K  -> k=17,  s=8
 */
 
-# define k 15 // number of bits for hashing branches
-# define s 5 // 4096=2^12.  12 bits for history
+# define k 17 // number of bits for hashing branches
+# define s 8  // 4096=2^12.  12 bits for history
 
 
 /*
 Stores global history table
 
-Maximum size
-k=12 -> 4096 = 2^12
-s=8  -> 256  = 2^8
+Maximum size: 2^20
  */
 unsigned int pattern_table[1048576];
 
 /*
 Histories for each branch
 
-Maximum size
-k=12 -> 4096 = 2^12
+Maximum size: 2^20
 */
 unsigned int histories[1048576];
 
+// mask for k bits
 unsigned int kmask;
+// mask for s bits
 unsigned int smask;
 
 void init_predictor ()
 {
-  printf("init");
 
   kmask = 0;
   smask = 0;
@@ -71,14 +69,9 @@ void init_predictor ()
 
 bool make_prediction (unsigned int pc)
 {
-  //printf("predict\n");
-  //unsigned int km = pow(2, k);
-  //unsigned int sm = pow(2, s);
-  //int km = 2;
   unsigned int branch = (pc & kmask);  // branch hash
-  //printf ("branch %d\n", branch);
   unsigned int i = histories[branch] & smask;
-  //printf ("histories %d\n", i);
+
   if(pattern_table[i] < 2)
     return true;
   else
@@ -88,38 +81,23 @@ bool make_prediction (unsigned int pc)
 
 void train_predictor (unsigned int pc, bool outcome)
 {
-  //printf("train\n");
-  //int km = 2;
-  //int sm = 2;
-  //unsigned int km = pow(2, k);
-  //unsigned int sm = pow(2, s);
   unsigned int branch = (pc & kmask);  // branch hash
   unsigned int i = histories[branch] & smask;
   unsigned int state = pattern_table[i];
 
-  //printf("branch idx:%d, pattern idx: %d, state: %d\n", branch, i, state);
-  if(pattern_table[i] == ST and outcome == true){
+  if (state < 2 and outcome){
     pattern_table[i] = ST;
-  } else if(pattern_table[i] == ST and outcome == false){
-    pattern_table[i] = WT;
-  }
-  // Weakly taken
-  else if(pattern_table[i] == WT and outcome == true){
-    pattern_table[i] = ST;
-  } else if(pattern_table[i] == WT and outcome == false){
-    pattern_table[i] = WNT;
-  }
-  // Weakly not taken
-  else if(pattern_table[i] == WNT and outcome == true){
-    pattern_table[i] = WT;
-  } else if(pattern_table[i] == WNT and outcome == false){
+
+  } else if (state >= 2 and not outcome){
     pattern_table[i] = SNT;
-  }
-  // Strongly not taken
-  else if(pattern_table[i] == SNT and outcome == true){
-    pattern_table[i] = WNT;
-  } else if(pattern_table[i] == SNT and outcome == false){
-    pattern_table[i] = SNT;
+
+  } else if (state >= 2 and outcome){
+    pattern_table[i] = MAX(0, state-1);
+
+  } else if (state < 2 and not outcome){
+    pattern_table[i] = MIN(3, state+1);
+  } else{
+    printf("warning\n");
   }
 
   histories[branch] =  ((histories[branch] << 1) + outcome) & smask;
