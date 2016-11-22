8f234f24cf74d4590191e55fbb3c16866a0a53b7
Jamie Morton <jamietmorton@gmail.com>
2016-11-21 16:19:12 -0800

    Updating local to actually be correct

--- local_predictor.C
+++ local_predictor.C
@@ -21,8 +21,8 @@ Budget sizes
 1M   + 4K  -> k=12, s=8
 */
 
-# define k 10 // number of bits for hashing branches
-# define s 19 // 4096=2^12.  12 bits for history
+# define k 15 // number of bits for hashing branches
+# define s 5 // 4096=2^12.  12 bits for history
 
 
 /*
@@ -42,17 +42,29 @@ k=12 -> 4096 = 2^12
 */
 unsigned int histories[1048576];
 
+unsigned int kmask;
+unsigned int smask;
 
 void init_predictor ()
 {
   printf("init");
+
+  kmask = 0;
+  smask = 0;
   // initialize all 2bit saturating counts to Strongly Taken
   for (int i = 0; i<pow(2, k); i++){
     // initialize all histories to zero.
-    histories[i] = 1;
+    histories[i] = 0;
   }
+  for (int i = 0; i < k; i++){
+    kmask = (kmask << 1) + 1;
+  }
+
   for (int j = 0; j<pow(2, s); j++){
-    pattern_table[j] = 3;
+    pattern_table[j] = 0;
+  }
+  for (int j = 0; j < s; j++){
+    smask = (smask << 1) + 1;
   }
 }
 
@@ -60,12 +72,12 @@ void init_predictor ()
 bool make_prediction (unsigned int pc)
 {
   //printf("predict\n");
-  unsigned int km = pow(2, k);
-  unsigned int sm = pow(2, s);
+  //unsigned int km = pow(2, k);
+  //unsigned int sm = pow(2, s);
   //int km = 2;
-  unsigned int branch = (pc % km);  // branch hash
+  unsigned int branch = (pc & kmask);  // branch hash
   //printf ("branch %d\n", branch);
-  unsigned int i = histories[branch] % sm;
+  unsigned int i = histories[branch] & smask;
   //printf ("histories %d\n", i);
   if(pattern_table[i] < 2)
     return true;
@@ -79,22 +91,36 @@ void train_predictor (unsigned int pc, bool outcome)
   //printf("train\n");
   //int km = 2;
   //int sm = 2;
-  unsigned int km = pow(2, k);
-  unsigned int sm = pow(2, s);
-  unsigned int branch = (pc % km);  // branch hash
-  unsigned int i = histories[branch] % sm;
+  //unsigned int km = pow(2, k);
+  //unsigned int sm = pow(2, s);
+  unsigned int branch = (pc & kmask);  // branch hash
+  unsigned int i = histories[branch] & smask;
   unsigned int state = pattern_table[i];
-  if (state < 2 and outcome){
+
+  //printf("branch idx:%d, pattern idx: %d, state: %d\n", branch, i, state);
+  if(pattern_table[i] == ST and outcome == true){
     pattern_table[i] = ST;
-  } else if (state >= 2 and not outcome){
+  } else if(pattern_table[i] == ST and outcome == false){
+    pattern_table[i] = WT;
+  }
+  // Weakly taken
+  else if(pattern_table[i] == WT and outcome == true){
+    pattern_table[i] = ST;
+  } else if(pattern_table[i] == WT and outcome == false){
+    pattern_table[i] = WNT;
+  }
+  // Weakly not taken
+  else if(pattern_table[i] == WNT and outcome == true){
+    pattern_table[i] = WT;
+  } else if(pattern_table[i] == WNT and outcome == false){
     pattern_table[i] = SNT;
-  } else if (state >= 2 and outcome){
-    pattern_table[i] = MAX(0, state-1);
-  } else if (state < 2 and not outcome){
-    pattern_table[i] = MIN(3, state+1);
-  } else{
-    printf("warning\n");
+  }
+  // Strongly not taken
+  else if(pattern_table[i] == SNT and outcome == true){
+    pattern_table[i] = WNT;
+  } else if(pattern_table[i] == SNT and outcome == false){
+    pattern_table[i] = SNT;
   }
 
-  histories[i] =  ((histories[i] << 1) | outcome) % sm;
+  histories[branch] =  ((histories[branch] << 1) + outcome) & smask;
 }
